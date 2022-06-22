3db5ed8ed68c6890210e36c132ed13d19b5a73c4
andreapasquale94 <andrea.pasquale@outlook.it>
2022-06-22 11:46:17 +0200

    Constants package init

---
+++ src/Constants/Constants.jl
@@ -0,0 +1,12 @@
+module Constants
+
+# Speed of light
+const SPEED_OF_LIGHT = 2.99792458e8 # m/s
+
+# Gravitational constant
+const G = 6.67428e-11 # m³/(kg * s²)
+
+# Astronomical unit 
+const AU = 1.49597870700e11 # m
+
+end
