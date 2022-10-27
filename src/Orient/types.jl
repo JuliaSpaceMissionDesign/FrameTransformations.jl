e075de68844a550b0529053aa30cc011d721a123
andreapasquale94 <andrea.pasquale@polimi.it>
2022-10-27 18:44:18 +0200

    minor

--- src/Orient/types.jl
+++ src/Orient/types.jl
@@ -1,11 +1,3 @@
-#
-# Copyright (c) 2018-2020 Helge Eichhorn and the AstroBase.jl contributors
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-
 export iau2006a, iau2006b
 
 abstract type IAUModel end
