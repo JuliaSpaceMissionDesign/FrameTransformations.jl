af19d1abe5bbad4be302f629f547639e18258661
MicheleCeresoli <michele.ceresoli@mail.polimi.it>
2022-10-30 17:05:26 +0100

    Documented fundamentals

--- src/Orient/Earth/fundamentals.jl
+++ src/Orient/Earth/fundamentals.jl
@@ -42,93 +42,343 @@ function FundamentalArguments(t::Number)
     )
 end
 
-# Computes mean anomaly of the moon 
+
+"""
+    fa_mano_moon(t::Number) 
+
+Returns the mean anomaly of the Moon 'Mₐ` in radians.  
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mano_moon(t::Number) 
     mod2pi(@evalpoly(t, 485868.249036, 
                         1717915923.2178, 
                         31.8792, 
                         0.051635, 
-                        -0.00024470) |> arcsec2rad)
+                        -0.00024470)*π/648000)
 end
 
 
-# Computes mean anomaly of the sun
+"""
+    fa_mano_sun(t::Number) 
+
+Returns the mean anomaly of the Sun `Sₐ` in radians.  
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mano_sun(t::Number) 
     mod2pi(@evalpoly(t, 1287104.793048, 
                         129596581.0481, 
                         -0.5532,
                         0.000136, 
-                        -0.00001149) |> arcsec2rad)
+                        -0.00001149)*π/648000)
 end
 
-# Computes mean longitude of the moon's ascending node  
+
+"""
+    fa_mlon_moon(t::Number) 
+
+Returns the mean longitude of the Moon's ascending node `Ωₘ` in radians.  
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mlon_moon(t::Number) 
     mod2pi(@evalpoly(t, 450160.398036, 
                         -6962890.54311, 
                         7.4722,
                         0.007702, 
-                        -0.00005939) |> arcsec2rad)
+                        -0.00005939)*π/648000)
 end
 
-# Computes mean argument of latitude of the Moon
+
+"""
+    fa_mlat_moon(t::Number) 
+
+Returns the mean longitude of the Moon minus the mean longitude of the 
+ascending node `uₘ` in radians.  
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mlat_moon(t::Number)
     mod2pi(@evalpoly(t, 335779.526232, 
                         1739527262.8478, 
                         -12.7512,
                         -0.001037, 
-                        -0.00000417) |> arcsec2rad)
+                        -0.00000417)*π/648000)
 end
 
-# Computes mean elongation of the Moon from the Sun 
+"""
+    fa_melo_moon(t::Number) 
+
+Returns the mean elongation of the Moon from the Sun `Dₛ` in radians. 
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_melo_moon(t::Number)
     mod2pi(@evalpoly(t, 1072260.70369 , 
                         1602961601.2090, 
                         -6.3706,
                         -0.006593, 
-                        -0.00003169) |> arcsec2rad)
+                        -0.00003169)*π/648000)
 end
 
-# Compute general accumulated precession in longitude 
+"""
+    fa_precession(t::Number) 
+
+Returns the general accumulated precession in longitude `pₐ` in radians.  
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_precession(t::Number)
     mod2pi(@evalpoly(t, 0, 0.024381750, 0.00000538691))
 end
 
-# Compute mean heliocentric longitude of Mercury 
+
+"""
+fa_mlon_mercury(t::Number) 
+
+Returns the mean heliocentric longitude of Mercury in radians. 
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mlon_mercury(t::Number)
     mod2pi(@evalpoly(t, 4.402608842, 2608.7903141574))
 end
 
-# Compute mean heliocentric longitude of Venus 
+
+"""
+fa_mlon_venus(t::Number) 
+
+Returns the mean heliocentric longitude of Venus in radians. 
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mlon_venus(t::Number)
     mod2pi(@evalpoly(t, 3.176146697, 1021.3285546211))
 end
 
-# Compute mean heliocentric longitude of Earth 
+
+"""
+fa_mlon_earth(t::Number) 
+
+Returns the mean heliocentric longitude of Earth in radians. 
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mlon_earth(t::Number)
     mod2pi(@evalpoly(t, 1.753470314, 628.3075849991))
 end
 
-# Compute mean heliocentric longitude of Mars 
+
+"""
+fa_mlon_mars(t::Number) 
+
+Returns the mean heliocentric longitude of Mars in radians. 
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mlon_mars(t::Number)
     mod2pi(@evalpoly(t, 6.203480913, 334.0612426700))
 end
 
-# Compute mean heliocentric longitude of Jupiter 
+
+"""
+fa_mlon_jupiter(t::Number) 
+
+Returns the mean heliocentric longitude of Jupiter in radians. 
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mlon_jupiter(t::Number)
     mod2pi(@evalpoly(t, 0.599546497, 52.9690962641))
 end
 
-# Compute mean heliocentric longitude of Saturn 
+
+"""
+fa_mlon_saturn(t::Number) 
+
+Returns the mean heliocentric longitude of Saturn in radians. 
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mlon_saturn(t::Number)
     mod2pi(@evalpoly(t, 0.874016757, 21.3299104960))
 end
 
-# Compute mean heliocentric longitude of Uranus 
+
+"""
+fa_mlon_uranus(t::Number) 
+
+Returns the mean heliocentric longitude of Uranus in radians. 
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mlon_uranus(t::Number)
     mod2pi(@evalpoly(t, 5.481293872, 7.4781598567))
 end
 
-# Compute mean heliocentric longitude of Neptune 
+
+"""
+fa_mlon_neptune(t::Number) 
+
+Returns the mean heliocentric longitude of Neptune in radians. 
+
+### Inputs 
+- `t`-- `TDB` Julian centuries since J2000.0 (see Notes)
+
+### Notes
+This implementation follows SOFA routines, where even though  `t` is strictly 
+expressed as TDB, it is usually more convenient to use Terrestrial Time (TT), 
+which makes no significant differences in the final result.
+
+### References 
+- Luzum, B. and Petit G. (2012). _The IERS Conventions (2010)_, 
+  [IERS Technical Note No. 36](https://www.iers.org/IERS/EN/Publications/TechnicalNotes/tn36.html) 
+- [ERFA](https://github.com/liberfa/erfa/blob/master/src/nut00b.c) software library
+"""
 function fa_mlon_neptune(t::Number)
     mod2pi(@evalpoly(t, 5.311886287, 3.8133035638))
 end
