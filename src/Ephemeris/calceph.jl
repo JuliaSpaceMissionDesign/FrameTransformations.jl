b9353607516aad5e5dfb13c26b6e0991829a57dd
MicheleCeresoli <michele.ceresoli@polimi.it>
2023-01-16 22:19:09 +0100

    Added ephemeris orientation functions

--- src/Ephemeris/calceph.jl
+++ src/Ephemeris/calceph.jl
@@ -3,7 +3,10 @@ export CalcephProvider,
        ephem_timescale, 
        ephem_position_records,
        ephem_orient_records,
-       ephem_compute_order!
+       ephem_available_points, 
+       ephem_available_axes,
+       ephem_compute_order!,
+       ephem_orient_order!
 
 using CALCEPH: Ephem as CalcephEphemHandler, 
                prefetch, 
@@ -11,8 +14,8 @@ using CALCEPH: Ephem as CalcephEphemHandler,
                timeScale,
                positionRecords,
                orientationRecords,
-               unsafe_compute!, 
-               useNaifId, unitKM, unitSec
+               unsafe_compute!, unsafe_orient!,
+               useNaifId, unitKM, unitSec, unitRad
 
 using Basic: AstronautGenericError, EphemerisError
 
@@ -45,12 +48,48 @@ ephem_position_records(eph::CalcephProvider) = positionRecords(eph.ptr)
 
 
 """
+    ephem_available_points(eph::CalcephProvider)
+
+Return a list of NAIFIds representing bodies with available ephemeris data. 
+"""
+function ephem_available_points(eph::CalcephProvider)
+    try 
+        rec = ephem_position_records(eph)
+        tids = map(x->x.target, rec)
+        cids = map(x->x.center, rec)
+
+        return unique([tids..., cids...])
+    catch 
+        return Int64[]
+    end
+end
+
+
+"""
     ephem_orient_records(eph::CalcephProvider)
 
 Get ephemeris an array of `CALCEPH.OrientationRecord`s, providing detailed 
 informations on the content of the ephemeris file.
 """
 ephem_orient_records(eph::CalcephProvider) = orientationRecords(eph.ptr)
+
+
+"""
+    ephem_available_points(eph::CalcephProvider)
+
+Return a list of Frame IDs representing axes with available orientation data. 
+"""
+function ephem_available_axes(eph::CalcephProvider)
+    try 
+        rec = ephem_orient_records(eph)
+        tids = map(x->x.target, rec)
+        cids = map(x->x.frame, rec)
+
+        return unique([tids..., cids...])
+    catch 
+        return Int64[]
+    end
+end
 
 """
     ephem_timespan(eph::CalcephProvider)
@@ -105,20 +144,29 @@ function ephem_timescale(eph::CalcephProvider)
     end
 end
 
+
 """
     ephem_compute_order!(res, eph, jd0, time, target, center, order)
 
-### Inputs
-- `res` -- results
-- `eph` -- ephemeris provider
-- `jd0` and `time` -- Two-parts Julian date
-"""
-function ephem_compute_order!(res, eph::AbstractEphemerisProvider, jd0, time, target, 
-    center, order) end
+Interpolate the position and/or its derivatives up to `order` for one body `target` relative 
+to another `center` at the time `jd0` + `time`, expressed as a Julian Date. This function reads 
+the ephemeris files associated to `eph` and stores the results to `res`.
 
-# TODO: jd0+time da scrivere come datetime!
+The returned array `res` must be large enough to store the results. The size of this array 
+must be equal to 3*order: 
+
+- res[1:3] contain the position (x, y, z) and is always valid 
+- res[4:6] contain the velocity (dx/dt, dy/dt, dz/dt) for order ≥ 1 
+- res[7:9] contain the acceleration (d²x/dt², d²y/dt², d²z/dt²) for order ≥ 2
+- res[10:12] contain the jerk (d³x/dt³, d³y/dt³, d³z/dt³) for order ≥ 3
+
+The values stores in `res` are always returned in km, km/s, km/s², km/s³
+
+### See also 
+See also [`ephem_orient_order!`](@ref)
+"""
 function ephem_compute_order!(res, eph::CalcephProvider, jd0::Float64, time::Float64, 
-    target::Int, center::Int, order::Int)
+            target::Int, center::Int, order::Int)
     stat = unsafe_compute!(res, eph.ptr, jd0, time, target, center, useNaifId+unitKM+unitSec, order)
     stat == 0 && throw(EphemerisError(String(Symbol(@__MODULE__)), "ephemeris data for "*
                     "point with NAIFId $target with respect to point $center is not available "*
@@ -126,11 +174,32 @@ function ephem_compute_order!(res, eph::CalcephProvider, jd0::Float64, time::Flo
     nothing
 end
 
-# function ephem_compute_order!(res, eph::SpiceProvider, jd0::Float64, time::Float64, 
-#     target::Int, center::Int, order::Int)
 
-#     et = ((jd0 + time) - DJ2000) * 86400
+"""
+    ephem_orient_order!(res, eph, jd0, time, target, order)
 
-#     unsafe_compute!(res, eph.ptr, jd0, time, target, center, useNaifId+unitKM+unitSec, order)
-#     nothing
-# end
+Interpolate the orientation and its derivatives up to `order` for the `target` body at the 
+time `jd0` + `time`, expressed as a Julian Date. This function reads the ephemeris files 
+associated to `eph` and stores the results to `res`.
+
+The returned array `res` must be large enough to store the results. The size of this array 
+must be equal to 3*order: 
+
+- res[1:3] contain the angles 
+- res[4:6] contain the 1st derivative  for order ≥ 1 
+- res[7:9] contain the 2nd derivative for order ≥ 2
+- res[10:12] contain the 3rd derivative for order ≥ 3
+
+The values stores in `res` are always returned in rad, rad/s, rad/s², rad/s³
+
+### See also 
+See also [`ephem_compute_order!`](@ref)
+"""
+function ephem_orient_order!(res, eph::CalcephProvider, jd0::Float64, time::Float64, 
+            target::Int, order::Int)
+
+    stat = unsafe_orient!(res, eph.ptr, jd0, time, target, useNaifId+unitRad+unitSec, order)
+    stat == 0 && throw(EphemerisError(String(Symbol(@__MODULE__)), "ephemeris data for "*
+                "frame with NAIFId $target is not available at JD $(jd0+time)"))
+    nothing
+end
