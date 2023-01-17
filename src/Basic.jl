1d80c2d1dee8456c0a8a4cf020184a5d833dad71
MicheleCeresoli <michele.ceresoli@mail.polimi.it>
2023-01-17 16:59:40 +0100

    Removed Bodies

--- src/Basic.jl
+++ src/Basic.jl
@@ -16,9 +16,6 @@ module Basic
     include(joinpath("Tempo", "Tempo.jl"))
     @reexport using .Tempo
 
-    include(joinpath("Bodies", "Bodies.jl"))
-    @reexport using .Bodies
-
     include(joinpath("Ephemeris", "Ephemeris.jl"))
     @reexport using .Ephemeris
 
