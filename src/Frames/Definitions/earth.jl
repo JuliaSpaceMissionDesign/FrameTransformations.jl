7da73eeeac8ec390c9b0cb01d3c33ecd30d89b6a
MicheleCeresoli <michele.ceresoli@polimi.it>
2023-01-23 10:43:10 +0100

    Fixed minors

--- src/Frames/Definitions/earth.jl
+++ src/Frames/Definitions/earth.jl
@@ -5,16 +5,19 @@ export add_axes_itrf!
 
 Add `axes` as a set of axes representing the International Terrestrial Reference Frame (ITRF)
 to `frames`. Only the :ICRF and :GCRF are accepted as `parent` axes. Use the `model` argument 
-to specify which IAU2010 convention (IAU2006A or IAU2000B) model should be used for the 
-computations. The default is set to `iau2006b`.
+to specify which IAU model model should be used for the computations. The default is set to 
+`iau2006b`.
+
+### See also 
+See also [`orient_rot3_itrf_to_gcrf`](@ref)
 """
 function add_axes_itrf!(frames::FrameSystem{O, T}, 
     axes::AbstractFrameAxes, parent::AbstractFrameAxes, 
-    model::Orient.IAU2006Model=Orient.iau2006b) where {T, O}
+    model::Orient.IAUModel=Orient.iau2006b) where {T, O}
 
     pname = axes_name(parent)
 
-    if !(pname in :ICRF, :GCRF) && axes_id(axes) != Orient.AXESID_ICRF
+    if !(pname in :ICRF, :GCRF) && axes_id(parent) != Orient.AXESID_ICRF
         throw(
             ErrorException("International Terrestrial Reference Frame (ITRF) axes could "*
             " not be defined w.r.t $pname axes. Only the `ICRF` or `GCRF` are accepted as"*
@@ -23,9 +26,10 @@ function add_axes_itrf!(frames::FrameSystem{O, T},
     end 
 
     add_axes_rotating!(frames, axes, parent, 
-                        t->Orient.orient_itrf_to_gcrf(t, model),
-                        t->Orient.orient_d_itrf_to_gcrf(t, model),
-                        t->Orient.orient_dd_itrf_to_gcrf(t, model)
+                        t->Orient.orient_rot3_itrf_to_gcrf(t, model),
+                        t->Orient.orient_rot6_itrf_to_gcrf(t, model),
+                        t->Orient.orient_rot9_itrf_to_gcrf(t, model),
+                        t->Orient.orient_rot12_itrf_to_gcrf(t, model)
     )
 
 end
