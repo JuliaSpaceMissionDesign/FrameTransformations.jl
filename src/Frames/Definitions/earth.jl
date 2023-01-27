370b51ac81dca706ebac560a26f90b9f04948fe8
MicheleCeresoli <michele.ceresoli@polimi.it>
2023-01-27 20:56:43 +0100

    Fixed minor

--- src/Frames/Definitions/earth.jl
+++ src/Frames/Definitions/earth.jl
@@ -17,7 +17,7 @@ function add_axes_itrf!(frames::FrameSystem{O, T},
 
     pname = axes_name(parent)
 
-    if !(pname in :ICRF, :GCRF) && axes_id(parent) != Orient.AXESID_ICRF
+    if !(pname in (:ICRF, :GCRF)) && axes_id(parent) != Orient.AXESID_ICRF
         throw(
             ErrorException("International Terrestrial Reference Frame (ITRF) axes could "*
             " not be defined w.r.t $pname axes. Only the `ICRF` or `GCRF` are accepted as"*
