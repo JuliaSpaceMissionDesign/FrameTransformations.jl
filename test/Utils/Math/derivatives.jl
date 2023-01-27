e5193dadf9964c40a77193b6dc2bf48e50b639c5
MicheleCeresoli <michele.ceresoli@polimi.it>
2023-01-27 18:21:56 +0100

    Fixed test tolerances

--- test/Utils/Math/derivatives.jl
+++ test/Utils/Math/derivatives.jl
@@ -4,8 +4,8 @@
     f = x-> sin(x) + 3x^3
     τ = rand()
 
-    @test D¹(f, τ) ≈ cos(τ) + 9τ^2
-    @test D²(f, τ) ≈ -sin(τ) + 18τ
-    @test D³(f, τ) ≈ -cos(τ) + 18
+    @test D¹(f, τ) ≈ cos(τ) + 9τ^2 atol=1e-14
+    @test D²(f, τ) ≈ -sin(τ) + 18τ atol=1e-14
+    @test D³(f, τ) ≈ -cos(τ) + 18  atol=1e-14
     
 end;
