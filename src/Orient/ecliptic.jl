2826a11d3e464e45d17834ab26318cecddc8acbb
MicheleCeresoli <michele.ceresoli@polimi.it>
2023-02-01 23:02:11 +0100

    Added note to Frame Bias

--- src/Orient/ecliptic.jl
+++ src/Orient/ecliptic.jl
@@ -42,10 +42,15 @@ const AXESID_ECLIPJ2000 = 17
 DCM for the rotation from the International Celestial Reference Frame (`ICRF`) and the 
 Mean Dynamical Equator and Equinox of J2000.0 (`MEME2000`).
 
+!!! note 
+    The frame bias is here computed using the IAU 2006 Precession model, similarly to ESA's 
+    GODOT. Some other software libraries, such as Orekit, use the frame bias of the IAU 2000 
+    precession model. The two definitions differ of about 1 arcsecond.
+
 ### References
 - Hilton, James L., and Catherine Y. Hohenkerk. -- Rotation matrix from the mean 
-dynamical equator and equinox at J2000. 0 to the ICRS. -- Astronomy & Astrophysics 
-413.2 (2004): 765-770. DOI: [10.1051/0004-6361:20031552](https://www.aanda.org/articles/aa/pdf/2004/02/aa3851.pdf)
+    dynamical equator and equinox at J2000. 0 to the ICRS. -- Astronomy & Astrophysics 
+    513.2 (2004): 765-770. DOI: [10.1051/0004-6361:20031552](https://www.aanda.org/articles/aa/pdf/2004/02/aa3851.pdf)
 - [SOFA docs](https://www.iausofa.org/2021_0512_C/sofa/sofa_pn_c.pdf)
 """
 const DCM_ICRF_TO_J2000_BIAS = orient_bias_precession(iau2006a, 0.0)
