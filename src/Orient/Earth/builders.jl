28475ca94454c980cc912a7e4e9251c37f1c253e
MicheleCeresoli <michele.ceresoli@polimi.it>
2023-11-12 21:19:28 +0100

    Updated series builders

---
+++ src/Orient/Earth/builders.jl
@@ -0,0 +1,337 @@
+
+# These functions are used to automatically generated optimised functions that 
+# compute the series associated to the IAU constants.
+
+# Used to map the IAUSeries coefficients to the fundamental argument name 
+const _FA_MAPPING = SVector(
+    :Mₐ, :Sₐ, :uₘ, :Dₛ, :Ωₘ, :λ_Me, :λ_Ve, :λ_Ea, :λ_Ma, :λ_Ju, :λ_Sa, :λ_Ur, :λ_Ne, :pₐ
+)
+
+function _assemble_function(iau_model, fname, fbody)
+
+    # Creates function call Expression
+    fcall = Expr(:call, fname)
+
+    push!(fcall.args, Expr(:(::), iau_model))   # Adds model
+    push!(fcall.args, Expr(:(::), :t, :Number)) # Adds time 
+    push!(fcall.args, Expr(:(::), :fa, :FundamentalArguments)) # Adds FA
+
+    # Assembles Function head and body
+    return Expr(:function, fcall, fbody)
+
+end
+
+function build_cio_series(
+    fname::Symbol,
+    iau_model::Symbol,
+    cpoly::AbstractVector{<:AbstractVector{<:Number}},
+    ctrig::AbstractVector{<:AbstractVector{<:AbstractVector{<:IAUSeries}}},
+)
+    nseries = length(cpoly)
+    if nseries != length(ctrig)
+        throw(ArgumentError("[Orient] cpoly and ctrig must have same number of series."))
+    end
+
+    # Generates ad-hoc functions to compute the IAU Series including only 
+    # the non-null coefficients. 
+
+    # The elements of cpoly must always be >= ctrig
+    fcn_body = Expr(:block)
+
+    # Pre-computes all the Fundamental Arguments multiplications
+    fad = Dict{Symbol,Vector{Int}}()
+    for (m, fm) in enumerate(_FA_MAPPING)
+        fad[fm] = Vector{Int}()
+
+        # Finds all the multiplicative factors of argument `fm`
+        for i in eachindex(ctrig)
+            for j in eachindex(ctrig[i])
+                for s in ctrig[i][j]
+                    if !(s.N[m] in fad[fm]) && s.N[m] != 0
+                        push!(fad[fm], s.N[m])
+                    end
+                end
+            end
+        end
+
+        # Register the expression for that facotr
+        for (k, mk) in enumerate(fad[fm])
+            fae = Expr(:(.), :fa, QuoteNode(fm))
+            push!(fcn_body.args, Expr(:(=), Symbol("$fm$k"), Expr(:call, :(*), mk, fae)))
+        end
+    end
+
+    # Final @evalpoly call expression (depends on nblocks)
+    pcall = Vector{Expr}(undef, nseries)
+    for j in 1:nseries
+        var = Symbol("x$(j)x")
+
+        # Automatically includes transformation to radians
+        pcall[j] = Expr(
+            :(=),
+            var,
+            Expr(
+                :call,
+                :(/),
+                Expr(:call, :(*), Expr(:macrocall, Symbol("@evalpoly"), :, :t), π * 1e-6),
+                648000,
+            ),
+        )
+    end
+
+    # Stores the indexes of all the contributions that have already been computed 
+    dct = Dict{Tuple{Int,Int},Vector{Int}}()
+
+    for j in 1:nseries
+        # Assings all the polynomial contributions
+        for i in eachindex(cpoly[j])
+            dct[(j, i)] = Vector{Int}() # initialises dictionary keys 
+            push!(fcn_body.args, Expr(:(=), Symbol("x$(j)x$i"), cpoly[j][i]))
+            push!(pcall[j].args[2].args[2].args[2].args, Symbol("x$(j)x$i"))
+        end
+    end
+
+    for jj in 1:nseries
+        for i in eachindex(ctrig[jj])
+            # Parses the trigonometric series
+            for s in ctrig[jj][i]
+
+                # Computes the ARGUMENT expression
+                argv = Vector{Symbol}()
+                for (j, n) in enumerate(s.N)
+                    if n != 0
+                        push!(
+                            argv,
+                            Symbol("$(_FA_MAPPING[j])$(argmin(abs.(fad[_FA_MAPPING[j]] .- n)))"),
+                        )
+                    end
+                end
+
+                arge = length(argv) > 0 ? Expr(:call, :(+), argv...) : 0.0
+
+                # Stores current block expression
+                blkₑ = Vector{Expr}()
+
+                # Re-parses the entire trigonometric coefficients vector to check 
+                # for elements with same arg coeffs 
+                cn, sn = false, false
+
+                for jjj in 1:nseries
+                    for ii in eachindex(ctrig[jjj])
+                        sc, cc = 0.0, 0.0
+
+                        for (k, sk) in enumerate(ctrig[jjj][ii])
+                            if sk.N == s.N && !(k in dct[(jjj, ii)])
+                                # Stores the current index
+                                push!(dct[(jjj, ii)], k)
+
+                                # Groups together all the sin\cos terms
+                                sc += sk.sc
+                                cc += sk.cc
+
+                                # Stores whether sin\cos are actually computed
+                                sn = sn ? sn : sk.sc != 0.0
+                                cn = cn ? cn : sk.cc != 0.0
+                            end
+                        end
+
+                        sumₑ = Expr(:call, :(+))
+                        sc != 0.0 && push!(sumₑ.args, Expr(:call, :(*), sc, :sarg))
+                        cc != 0.0 && push!(sumₑ.args, Expr(:call, :(*), cc, :carg))
+
+                        if sc != 0.0 || cc != 0.0
+                            push!(blkₑ, Expr(:(+=), Symbol("x$(jjj)x$ii"), sumₑ))
+                        end
+                    end
+                end
+
+                # Adds all the expressions 
+                if cn || sn
+
+                    # Reassigns the arg variable
+                    push!(fcn_body.args, Expr(:(=), :arg, arge))
+
+                    # Computes and stores sin(ARG) and cos(ARG)
+                    cn && push!(fcn_body.args, Expr(:(=), :carg, Expr(:call, :cos, :arg)))
+                    sn && push!(fcn_body.args, Expr(:(=), :sarg, Expr(:call, :sin, :arg)))
+
+                    push!(fcn_body.args, blkₑ...)
+                end
+            end
+        end
+    end
+
+    for j in 1:nseries
+        push!(fcn_body.args, pcall[j])
+    end
+
+    # Adds return types
+    if nseries > 1
+        rtupl = Expr(:tuple)
+        for j in 1:nseries
+            push!(rtupl.args, Symbol("x$(j)x"))
+        end
+
+        push!(fcn_body.args, Expr(:return, rtupl))
+    end
+
+    # Assembles the function
+    fcn = _assemble_function(iau_model, fname, fcn_body)
+    return eval(fcn)
+end
+
+
+function build_nutation_series(
+    fname::Symbol, iau_model::Symbol, lon::AbstractVector{N}, obl::AbstractVector{N}
+) where {N <: AbstractVector{<:IAUSeries}}
+
+    # Generates ad-hoc functions to compute the IAU Series including only 
+    # the non-null coefficients. 
+
+    # ψseries and ϵseries must have the same number of elements (should be 2)
+    nblk = length(lon)
+    if nblk != length(obl)
+        throw(ArgumentError("The longitude and obliquity series must have the same order."))
+    end
+
+    fbody = Expr(:block)
+
+    # Pre-computes all the Fundamental Arguments multiplications
+    fad = Dict{Symbol,Vector{Int}}()
+    for (m, fm) in enumerate(_FA_MAPPING)
+
+        fad[fm] = unique(vcat([map(x->x.N[m], s[i]) for i = 1:nblk for s in (lon, obl)]...))
+        filter!(x-> x != 0, fad[fm])
+
+        # Register the expression for that facotr
+        for (k, mk) in enumerate(fad[fm])
+            fae = Expr(:(.), :fa, QuoteNode(fm))
+            push!(fbody.args, Expr(:(=), Symbol("$fm$k"), Expr(:call, :(*), mk, fae)))
+        end
+    end
+
+    # Final @evalpoly call expression (depends on nblk)
+    pcall = Vector{Expr}(undef, 2)
+    for (j, var) in enumerate((:dψ, :dϵ))
+        
+        # Automatically includes transformation to radians
+        pcall[j] = Expr(
+            :(=),
+            var,
+            Expr(
+                :call,
+                :(/),
+                Expr(:call, :(*), Expr(:macrocall, Symbol("@evalpoly"), :, :t), π * 1e-6),
+                648000,
+            ),
+        )
+    end
+
+    # Stores the indexes of all the contributions that have already been computed 
+    # First Tuple index refers to ψ or ϵ, the second to the corresponding block
+    dct = Dict{Tuple{Int,Int},Vector{Int}}()
+
+    # Initialises the variables 
+    for i in 1:nblk
+        # Adds ψ and ϵ contributions 
+        for (j, var) in enumerate((Symbol("dψ$(i-1)"), Symbol("dϵ$(i-1)"))) 
+            dct[(j, i)] = Vector{Int}() 
+            push!(fbody.args, Expr(:(=), var, 0))
+            push!(pcall[j].args[2].args[2].args[2].args, var)
+        end
+    end
+
+    for i in 1:nblk
+        for series in (lon, obl)
+
+            # Parses ψ or ϵ block 
+            for s in series[i]
+                
+                # for (j, n) in enumerate(s.N)
+                #     if n != 0
+                #         push!(
+                #             argv,
+                #             Symbol("$(_FA_MAPPING[j])$(argmin(abs.(fad[_FA_MAPPING[j]] .- n)))"),
+                #         )
+                #     end
+                # end
+
+                # Computes the ARGUMENT expression
+                argv = Vector{Symbol}()
+                for (fa, n) in zip(_FA_MAPPING, s.N)
+                    if n != 0 
+                        push!(argv, Symbol("$(fa)$(argmin(abs.(fad[fa] .- n)))"))
+                    end
+                end
+
+                arge = length(argv) > 0 ? Expr(:call, :(+), argv...) : 0
+
+                # Stores current block expression
+                blkₑ = Vector{Expr}()
+
+                # Re-parses the entire ψ and ϵ vectors to check 
+                # for elements with same arg coeffs 
+                cn, sn = false, false
+
+                for ii in 1:nblk
+
+                    # Parse ψ and ϵ
+                    for (v, vct) in enumerate([lon, obl])
+                        sc, cc = 0.0, 0.0
+
+                        # Parses the inner series 
+                        for (k, sk) in enumerate(vct[ii])
+                            if sk.N == s.N && !(k in dct[(v, ii)])
+
+                                # Stores the current index
+                                push!(dct[(v, ii)], k)
+
+                                # Groups together all the sin\cos terms
+                                sc += sk.sc
+                                cc += sk.cc
+
+                                # Stores whether sin\cos are actually computed
+                                sn = sn ? sn : sk.sc != 0.0
+                                cn = cn ? cn : sk.cc != 0.0
+                            end
+                        end
+
+                        sumₑ = Expr(:call, :(+))
+                        sc != 0.0 && push!(sumₑ.args, Expr(:call, :(*), sc, :sarg))
+                        cc != 0.0 && push!(sumₑ.args, Expr(:call, :(*), cc, :carg))
+
+                        if sc != 0.0 || cc != 0.0
+                            var = v == 1 ? "ψ" : "ϵ"
+                            push!(blkₑ, Expr(:(+=), Symbol("d" * var * "$(ii-1)"), sumₑ))
+                        end
+                    end
+                end
+
+                # Adds all the expressions 
+                if cn || sn
+
+                    # Reassigns the arg variable
+                    push!(fbody.args, Expr(:(=), :arg, arge))
+
+                    # Computes and stores sin(ARG) and cos(ARG)
+                    cn && push!(fbody.args, Expr(:(=), :carg, Expr(:call, :cos, :arg)))
+                    sn && push!(fbody.args, Expr(:(=), :sarg, Expr(:call, :sin, :arg)))
+
+                    push!(fbody.args, blkₑ...)
+                end
+            end
+        end
+    end
+
+    for i in 1:2
+        push!(fbody.args, pcall[i])
+    end
+
+    # Adds return types 
+    push!(fbody.args, Expr(:return, Expr(:tuple, :dψ, :dϵ)))
+
+    # Assembles the function
+    fcn = _assemble_function(iau_model, fname, fbody)
+    # return eval(fcn)
+
+end
--- src/Orient/Earth/gmst.jl
+++ src/Orient/Earth/gmst.jl
@@ -1,17 +1,176 @@
 export gmst
 
-"""
-    gmst(θ::Number, t::Number)
 
-Compute the Greenwich Mean Sidereal Time from the Earth rotation angle `Θ` and the epoch  `t` 
-expressed in `TT` Julian centuries since `J2000`. 
 """
-function gmst(θ::Number, t::Number)
-    c₀ = 0.014506
-    c₁ = 4612.156534
-    c₂ = 1.3915817
-    c₃ = -0.00000044
-    c₄ = -0.000029956
-    c₅ = -0.0000000368
-    return mod(86400 * θ + @evalpoly(t, c₀, c₁, c₂, c₃, c₄, c₅)/15, 86400)
+    gmst(m::IAUModel, t::Number, θ::Number)
+
+Compute the Greenwich Mean Sidereal Time (GMST), in radians, according to the IAU Model `m`, 
+given the Earth Rotation Angle (ERA) `θ`, in radians and the time `t` expressed in `TT` 
+Julian centuries since `J2000`. 
+
+The function has been implemented for the IAU2000 and IAU2006 models.
+
+### References 
+- [ERFA gmst00](https://github.com/liberfa/erfa/blob/master/src/gmst00.c) routine.
+- [ERFA gmst06](https://github.com/liberfa/erfa/blob/master/src/gmst06.c) routine.
+"""
+function gmst(::IAU2006Model, t::Number, θ::Number, )
+
+    # Evaluate interpolating series
+    p = @evalpoly(
+        t, 
+        0.014506, 
+     4612.156534,   
+        1.3915817, 
+       -0.00000044,
+       -0.000029956, 
+       -0.0000000368
+    )
+
+    # Compute GMST 
+    return mod2pi(θ + arcsec2rad(p))
+
+end
+
+function gmst(::IAU2000Model, t::Number, θ::Number, )
+
+    # Evaluate interpolating series
+    p = @evalpoly(
+        t, 
+        0.014506, 
+     4612.15739966,   
+        1.39667721, 
+       -0.00009344,
+       -0.00001882
+    )
+
+    # Compute GMST 
+    return mod2pi(θ + arcsec2rad(p))
+
+end
+
+"""
+Compute the Greenwich Mean Sidereal Time (GMST), in radians, according to the IAU 1980 
+models, given time `t` expressed in `TT` Julian centuries since `J2000`. 
+
+### References 
+- [ERFA gmst82](https://github.com/liberfa/erfa/blob/master/src/gmst82.c) routine.
+"""
+function gmst(::IAU1980Model, t::Number)
+
+    # Coefficients for the IAU 1982 GMST-UT1 model. The first component has been adjusted 
+    # of 12 hours because UT1 starts at noon.
+    A = -19089.45159
+    B = 8640184.812866
+    C = 0.093104
+    D = -6.2e-6
+
+    # Fractional part of UT1, in seconds
+    f = Tempo.CENTURY2SEC*mod(t, 1)
+
+    # seconds to radians conversion 
+    s2r = 7.272205216643039903848712e-5
+    
+    # Compute GMST
+    return mod2pi(s2r*(@evalpoly(t, A, B, C, D) + f))
+
+end
+
+
+"""
+Compute the Greenwich Apparent Sidereal Time (GAST), in radians, given time `t` as `TT` 
+Julian centuries since `J2000`
+"""
+function gast(m::IAU2006Model, t::Number, θ::Number)
+
+    # TODO: or maybe its the transpose?
+    NPB = orient_bias_precession_nutation(m, t)
+    
+    # Get the CIO locator 
+    s = cip_coords(m, t)
+
+    # Compute the equations of the origins
+    eors = origins_equation()  
+
+    return θ - eors
+end
+
+
+function gast(m::IAU2000Model, t::Number, θ::Number)
+
+    # Compute the Greenwich Mean Sidereal time 
+    gmst = gmst(m, θ, t)
+
+    # Compute the equations of the equinoxes
+    ee = equinoxes_equation(m, t)
+
+    # Comput GAST, in radians
+    return mod2pi(gmst + ee)
+
+end
+
+
+function gast(::IAU1980Model)
+
+    # Compute the Greenwich Mean Sidereal time 
+    gmst = gmst(m, t)
+
+    # Compute the equations of the equinoxes
+    ee = equinoxes_equation(m, t)
+
+    # Comput GAST, in radians
+    return mod2pi(gmst + ee)
+
+end
+
+
+# Time expressed in TDB julian centuries since J2000
+function equinoxes_equation(m::IAU2006Model, t::Number)
+
+    # Compute GMST and GAST, in radians 
+    gmst = gmst(m, t, θ)
+    gast = gast(m, t, θ)
+
+    # Equation of the equinoxes
+    return mod2pi(gast - gmst)
+
+end
+
+# Time expressed in TT julian centuries since J2000
+function equinoxes_equation(m::IAU2000Model, t::Number)
+
+    # Compute precession-rate adjustments 
+
+    # Compute the mean obliquity 
+    ϵₐ = orient_obliquity(m, t)
+
+    # Nutation in logitutude 
+
+    # Equation of the equinoxes
+    # TODO: here you have to implemetn a damned series 
+    return Δψ*cos(ϵₐ)
+
+end
+
+# Time expressed in TDB julian centuries since J2000
+function equinoxes_equation(m::IAU1980Model, t::Number)
+
+    # Longitude of the mean ascending node of the lunar orbit on the ecliptic, 
+    # measured from the mean equinox of date, in radians
+    ω = mod2pi(arcsec2rad(@evalpoly(t, -482890.539, 7.455, 0.008)) + 2π*mod(-5t, 1))
+
+    # Compute the nutation components in longitude and obliquity
+    # TODO: missing orient_nutation per il 1980 missa 
+
+    # Compute the mean obliquity 
+    ϵₐ = orient_obliquity(m, t)
+
+    # Equations of the equinoxes 
+    return Δψ*cos(ϵₐ) + arcsec2rad(0.00264*sin(ω) + 0.000063*sin(2ω))
+
+end
+
+
+function origins_equation(::IAU2006Model, t::Number)
+
 end
