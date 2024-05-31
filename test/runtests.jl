3d7b59f61a018df3ae3842fe1cbb38abeb6239dd
Andrea Pasquale <andrea.pasquale@polimi.it>
2024-05-31 11:49:14 +0200

    update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,12 @@
 using Test 
+
+const GROUP = get(ENV, "GROUP", "All")
+
+@time begin
+    if GROUP == "All" || GROUP == "Core"
+        include("Core/Core.jl")
+    end
+    if GROUP == "All" || GROUP == "Definitions"
+        include("Definitions/Definitions.jl")
+    end
+end
